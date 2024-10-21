228fdffcdce9aa14d2a6b0cf05329a9ac0902c71
Maksym025 <Maksym.Malashchuk.IK.2024@lpnu.ua>
2024-10-21 17:23:23 +0300

    Add files via upload

---
+++ Lab_07.1i/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_07.1i/Lab_07.1i.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[i][j]={2,3,4}
+			int S = 0;
+			int k = 0;
+			const int rowCount = 8;
+			const int colCount = 5;
+			int calc = Calc(a,rowCount,colCount,S,k)
+		}
+	};
+}
